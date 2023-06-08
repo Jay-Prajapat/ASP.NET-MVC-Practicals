e3d9b1078ca631bc0efc13923acfe1a5d1dc6bbf
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-06-08 10:21:58 +0530

    Completed Practical1
    Task1: Displayed Hello world Message.
    Task2: Using jQuery and different button change the font style, font-weight and decoration.
    Task3: Displayed "Hello world" message and unit test it.

---
+++ Practical-9/Practical9-Task1/UnitTestTask3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTask3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTask3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e9e5f739-b8c5-4c53-99a6-2b56778980b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Practical-9/Practical9-Task1/UnitTestTask3/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practical9_Task1.Controllers;
+using System;
+
+namespace UnitTestTask3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestHelloMessage()
+        {
+            Task3Controller controller = new Task3Controller();
+            var result = controller.Index();
+            Assert.AreEqual(result, "Hello World");
+        }
+    }
+}
