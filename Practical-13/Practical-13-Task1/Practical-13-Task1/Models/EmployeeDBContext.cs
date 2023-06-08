6856506d127c04ea38f6d5d052be1a4c34630665
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-06-08 19:26:14 +0530

    Completed Practical-13 Task-1
    Created MVC project and using code first migration and implemented CRUD operations.

---
+++ Practical-13/Practical-13-Task1/Practical-13-Task1/Models/EmployeeDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practical_13_Task1.Models
+{
+    public class EmployeeDBContext:DbContext
+    {
+        public EmployeeDBContext() : base("name=EmployeeDBContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<EmployeeDBContext, Practical_13_Task1.Migrations.Configuration>());
+        }
+        public DbSet<Employee> Employees { get; set;}
+    }
+}
