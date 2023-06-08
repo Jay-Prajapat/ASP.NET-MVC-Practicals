36fa5596981518ae9e063ddc65f76ad8a4df77f7
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-06-08 12:18:22 +0530

    Completed Practical 10
    Task-1: Create action method and declare Route that output the given name from Employee/Name url.
    
    Task-2: Implemented different action results like ContentResult, FileContentResult, EmptyResult, JavaScriptResult, JsonResult, etc.
    
    Task-3: Use of OutputCache filter, that action  method should cache current time for 5 minuits.
    
    Task-4: Implemented exception filter that handle the DivideByZeroException

---
+++ Practical-10/Practical-10/Practical-10/Controllers/Task4Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical_10.Controllers
+{
+    public class Task4Controller : Controller
+    {
+        // GET: Task4
+        [HandleError(ExceptionType = typeof(DivideByZeroException), View= "DivideByZero")]
+        
+        public ActionResult Index()
+        {
+            throw new DivideByZeroException();
+        }
+        
+    }
+}
