abcee37e223f803c73494f019edd475d11823f88
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-06-08 15:16:18 +0530

    Completed Practical-11
    Task1: Created CRUD application using static list of person details.
    
    Task2: Create CRUD application using static list of person details and used partial views for Create, Update and Delete.

---
+++ Practical-10/Practical-11/Practical-11/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practical_11.Models
+{
+    public class Person
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Address { get; set; }  
+    }
+}
